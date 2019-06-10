296dd3ee72a30ddb522f9d7959312e7243cc0a32
MH-Zarkhani <m.hassan.zarkhani@gmail.com>
2019-06-10 21:22:07 +0430

    Add Gallery section

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,28 +10,48 @@ $(document).ready(function() {
   $(window).scroll(function() {
     let currentPosition = $(this).scrollTop();
     // navbar-menu when scrolled
-    console.log(currentPosition);
-    
-    if(currentPosition > 200) {
+    if (currentPosition > 200) {
       $("#nav-menu").addClass("custom-navbar");
     } else {
       $("#nav-menu").removeClass("custom-navbar");
     }
-    if(currentPosition > 500) {
-      $('#nav-menu').addClass("fade");
-      if( previousPosition > currentPosition){
-        $('#nav-menu').removeClass("fade");
-      }else{
-        $('#nav-menu').addClass("fade");
+    if (currentPosition > 500) {
+      $("#nav-menu").addClass("fade");
+      if (previousPosition > currentPosition) {
+        $("#nav-menu").removeClass("fade");
+      } else {
+        $("#nav-menu").addClass("fade");
       }
     }
-    if(currentPosition > 650) {
-      $('.camera-img').addClass('fromLeft');
-      $('.camera-text').addClass('fromRight');
-    }else {
-      $('.camera-img').removeClass('fromLeft');
-      $('.camera-text').removeClass('fromRight');
+    if (currentPosition > 650) {
+      $(".camera-img").addClass("fromLeft");
+      $(".camera-text").addClass("fromRight");
+    } else {
+      $(".camera-img").removeClass("fromLeft");
+      $(".camera-text").removeClass("fromRight");
     }
     previousPosition = currentPosition;
+  });
+
+  // gallery
+
+  $(".gallery-list-item").on("click", function() {
+    // active-item class
+    $(this)
+      .addClass("active-item")
+      .siblings()
+      .removeClass("active-item");
+    // show selected items
+    let value = $(this).attr("data-filter");
+    if (value === "all") {
+      $(".filter").show(300);
+    } else {
+      $(".filter")
+        .not("." + value)
+        .hide(300);
+      $(".filter")
+        .filter("." + value)
+        .show(300);
+    }
   });
 });
